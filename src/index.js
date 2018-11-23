ae3eb7323e8d6f9ae391a4d7fccd5a62309372b9
Humenko <danielhumenko@gmail.com>
2018-11-23 10:45:00 +0000

    add scss

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import ScrollToTop from './components/Shared/ScrollToTop';
 import registerServiceWorker from './registerServiceWorker';
-import './styles/css/main.css';
+import './styles/sass/main.scss';
 
 ReactDOM.hydrate(
   <Router>
