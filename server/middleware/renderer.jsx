70d83b59c13053581a4f4b29b2bf51ced89bc2d4
Humenko <danielhumenko@gmail.com>
2018-11-24 13:04:09 +0000

    yarn

--- server/middleware/renderer.jsx
+++ server/middleware/renderer.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 import { StaticRouter as Router } from 'react-router-dom';
-// import our main App component
 import App from '../../src/App';
 
 const path = require('path');
 const fs = require('fs');
 
 export default (req, res, next) => {
-  // point to the html file created by CRA's build tool
   const filePath = path.resolve(__dirname, '..', '..', 'build', 'index.html');
 
   fs.readFile(filePath, 'utf8', (err, htmlData) => {
@@ -18,14 +16,12 @@ export default (req, res, next) => {
     }
     const context = {};
 
-    // render the app as a string
     const html = ReactDOMServer.renderToString(
       <Router location={req.baseUrl} context={context}>
         <App />
       </Router>
     );
 
-    // inject the rendered app into our html and send it
     return res.send(
       htmlData.replace('<div id="root"></div>', `<div id="root">${html}</div>`)
     );
