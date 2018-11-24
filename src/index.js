094fb5db2c10b2a818759ee13bb29ee7a1f1197a
Humenko <danielhumenko@gmail.com>
2018-11-24 15:39:59 +0000

    remove scroll

--- src/index.js
+++ src/index.js
@@ -2,15 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
-import ScrollToTop from './components/Shared/ScrollToTop';
 import registerServiceWorker from './registerServiceWorker';
 import './styles/sass/main.scss';
 
 ReactDOM.render(
   <Router>
-    <ScrollToTop>
-      <App />
-    </ScrollToTop>
+    <App />
   </Router>,
   document.getElementById('root')
 );
