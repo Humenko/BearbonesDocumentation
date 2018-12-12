32d33aafc25c2bba27dbba98c3530fed1c629268
Humenko <danielhumenko@gmail.com>
2018-12-12 15:33:57 +0000

    add copy animation

--- src/components/Shared/DocumentationHelper.jsx
+++ src/components/Shared/DocumentationHelper.jsx
@@ -2,19 +2,49 @@ import React, { Component } from 'react';
 import Copy from 'react-copy';
 
 export default class DocumentationHelper extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showResults: false
+    };
+
+    this.handleCopy = this.handleCopy.bind(this);
+  }
+
+  handleCopy() {
+    this.setState({
+      showResults: true
+    });
+    setTimeout(() => {
+      this.setState({
+        showResults: false
+      });
+    }, 2000);
+  }
+
   render() {
     return (
-      <div className={`bb-box example ${this.props.className}`}>
+      <div
+        className={`bb-box example ${
+          this.props.className ? this.props.className : ''
+        }`}
+      >
         {this.props.content && (
           <div className="doc-preview">{this.props.content}</div>
         )}
         <div className="doc-snippet">
-          <Copy textToBeCopied={this.props.code}>
-            <button className="bb-btn primary doc-copy sm">Copy</button>
+          <Copy textToBeCopied={this.props.code} onCopy={this.handleCopy}>
+            <button
+              className={`bb-btn primary doc-copy sm ${
+                this.state.showResults ? 'positive' : 'primary'
+              }`}
+            >
+              {this.state.showResults ? 'Copied' : 'Copy'}
+            </button>
           </Copy>
         </div>
       </div>
     );
   }
 }
-DocumentationHelper.propTypes = {};
