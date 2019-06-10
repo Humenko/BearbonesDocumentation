9752006997bef769a2c219990b066ce915733e0b
Daniel Humenko <danielhumenko@gmail.com>
2019-06-10 12:48:47 +0100

    add custom PWA capability

---
+++ src/sw-template.js
@@ -0,0 +1,39 @@
+if ('function' === typeof importScripts) {
+  importScripts(
+    'https://storage.googleapis.com/workbox-cdn/releases/3.5.0/workbox-sw.js',
+  );
+  /* global workbox */
+  if (workbox) {
+    console.log('Workbox is loaded');
+
+    /* force update PWA*/
+    workbox.skipWaiting();
+    workbox.clientsClaim();
+
+    /* Google Analytics offline */
+    workbox.googleAnalytics.initialize();
+
+    /* injection point for manifest files.  */
+    workbox.precaching.precacheAndRoute([]);
+
+    /* custom cache rules*/
+    workbox.routing.registerNavigationRoute('/index.html', {
+      blacklist: [/^\/_/, /\/[^\/]+\.[^\/]+$/],
+    });
+
+    workbox.routing.registerRoute(
+      /\.(?:png|gif|jpg|jpeg)$/,
+      workbox.strategies.cacheFirst({
+        cacheName: 'images',
+        plugins: [
+          new workbox.expiration.Plugin({
+            maxEntries: 60,
+            maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+          }),
+        ],
+      }),
+    );
+  } else {
+    console.log('Workbox could not be loaded. No Offline support');
+  }
+}
